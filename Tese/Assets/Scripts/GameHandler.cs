5a8b92df5c68b2374f347827f5f70842b8e665b7
Diogo Alves <43208915+Drca1997@users.noreply.github.com>
2021-03-15 19:41:30 +0000

    Graphics Manager
    Para meter Renderizaçao de visuais à parte

--- Tese/Assets/Scripts/GameHandler.cs
+++ Tese/Assets/Scripts/GameHandler.cs
@@ -21,6 +21,7 @@ public class GameHandler : MonoBehaviour
     private Grid grid;
     private List <GameObject> spriteRenderers;
     private List<Bomb> bombs;
+    private GraphicsManager graphicsManager;
 
     // Start is called before the first frame update
     void Start()
@@ -28,54 +29,27 @@ public class GameHandler : MonoBehaviour
         grid = new Grid(width, width, new Vector3(origin[0], origin[1], origin[2]), cellSize,randomizeMap);
         grid.DisplayGrid();
         grid.DebugPrintGrid();
-        GraphicsSetup();
         bombs = new List<Bomb>();
+        graphicsManager = new GraphicsManager(grid, sprites);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetMouseButtonDown(0))
         {
+            ProcessAgentsActions();
             CheckBombs();
-            UpdateGraphics();
+            graphicsManager.UpdateGraphics();
         }
-
-        
-        
     }
 
-    void GraphicsSetup()
+    void ProcessAgentsActions()
     {
-        spriteRenderers = new List<GameObject>();
-        for (int i = 0; i < grid.Array.GetLength(0); i++)
-        {
-            for (int j = 0; j < grid.Array.GetLength(1); j++)
-            {
-                spriteRenderers.Add(Utils.CreateSpriteRenderer(grid.GetWorldPosition(i, j), sprites[grid.Array[i,j]], cellSize));
-            }
-        }
+
     }
 
-    void UpdateGraphics()
-    {
-        for (int i=0; i < grid.Array.GetLength(0); i++)
-        {
-            for (int j =0; j < grid.Array.GetLength(1); j++)
-            {
-
-                SpriteRenderer sR = spriteRenderers[i * grid.Array.GetLength(1) + j].GetComponent<SpriteRenderer>();
-                if (sR == null)
-                {
-                    Debug.Log("ERRO: TILE NAO ENCONTRADA!");
-                }
-                else
-                {
-                    sR.sprite = sprites[grid.Array[i, j]];
-                }
-            }
-        }
-    }
     void CheckBombs()
     {
         foreach (Bomb bomba in bombs)
---
+++ Tese/Assets/Scripts/GraphicsManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicsManager
+{
+
+    Grid grid;
+    Sprite[] sprites;
+    List<GameObject> spriteRenderers;
+
+    public GraphicsManager(Grid grid, Sprite [] sprites)
+    {
+        this.grid = grid;
+        this.sprites = sprites;
+        GraphicsSetup();
+    }
+
+    private void GraphicsSetup()
+    {
+        spriteRenderers = new List<GameObject>();
+        for (int i = 0; i < grid.Array.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.Array.GetLength(1); j++)
+            {
+                spriteRenderers.Add(Utils.CreateSpriteRenderer(grid.GetWorldPosition(i, j), sprites[grid.Array[i, j]], cellSize));
+            }
+        }
+    }
+
+    public void UpdateGraphics()
+    {
+        for (int i = 0; i < grid.Array.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.Array.GetLength(1); j++)
+            {
+
+                SpriteRenderer sR = spriteRenderers[i * grid.Array.GetLength(1) + j].GetComponent<SpriteRenderer>();
+                if (sR == null)
+                {
+                    Debug.Log("ERRO: TILE NAO ENCONTRADA!");
+                }
+                else
+                {
+                    sR.sprite = sprites[grid.Array[i, j]];
+                }
+            }
+        }
+    }
+}
