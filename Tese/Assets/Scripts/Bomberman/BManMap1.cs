e3191b7b7aec1cab76c03f9231f5132edb7f06b3
Bernardo Correia <bernardonunescorreia@gmail.com>
2021-05-03 20:54:23 +0100

    mapdate

--- Tese/Assets/Scripts/Bomberman/BManMap1.cs
+++ Tese/Assets/Scripts/Bomberman/BManMap1.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class BManMap1 : MonoBehaviour, IGenerateMap
 {
+    [MinAttribute (5)]
+    public int width = 15;
+    [MinAttribute (5)]
+    public int height = 13;
+    [Range(0,100)]
+    public int wallPercent = 60;
+
     public List<int>[,] GenerateMap(System.Random prng) {
 
-        List<int>[,] setup_grid = new List<int>[15,13];
+        List<int>[,] setup_grid = new List<int>[width, height];
 
         for (int x = 0; x < setup_grid.GetLength(0); x++)
         {
@@ -17,30 +24,30 @@ public class BManMap1 : MonoBehaviour, IGenerateMap
             }
         }
 
-        int[,] setup_setup_grid = new int[15, 13];
+        int[,] setup_setup_grid = new int[width, height];
 
-        List<Vector2Int> randomWall = Utils.OffsetPattern(Utils.RandomizePattern(Utils.PatternFilledBox(13, 11), 60, prng), new Vector2Int(1,1));
+        List<Vector2Int> randomWall = Utils.OffsetPattern(Utils.RandomizePattern(Utils.PatternFilledBox(width-2, height-2), wallPercent, prng), new Vector2Int(1,1));
 
         Utils.PutOnGrid(setup_setup_grid, randomWall, 1);
 
-        List<Vector2Int> freeSpace = Utils.OffsetPattern(Utils.PatternCross(2), new Vector2Int(1, 1));
-        freeSpace = Utils.MergePatterns(freeSpace, Utils.OffsetPattern(Utils.PatternCross(2), new Vector2Int(1, 11)));
-        freeSpace = Utils.MergePatterns(freeSpace, Utils.OffsetPattern(Utils.PatternCross(2), new Vector2Int(13, 1)));
-        freeSpace = Utils.MergePatterns(freeSpace, Utils.OffsetPattern(Utils.PatternCross(2), new Vector2Int(13, 11)));
+        List<Vector2Int> freeSpace = Utils.OffsetPattern(Utils.PatternCross(1), new Vector2Int(1, 1));
+        freeSpace = Utils.MergePatterns(freeSpace, Utils.OffsetPattern(Utils.PatternCross(1), new Vector2Int(1, height - 2)));
+        freeSpace = Utils.MergePatterns(freeSpace, Utils.OffsetPattern(Utils.PatternCross(1), new Vector2Int(width - 2, 1)));
+        freeSpace = Utils.MergePatterns(freeSpace, Utils.OffsetPattern(Utils.PatternCross(1), new Vector2Int(width - 2, height - 2)));
 
         Utils.PutOnGrid(setup_setup_grid, freeSpace, 0);
 
-        List<Vector2Int> strongWall = Utils.OffsetPattern(Utils.PatternFilledBox(12,10,1,1,1,1), new Vector2Int(2, 2));
+        List<Vector2Int> strongWall = Utils.OffsetPattern(Utils.PatternFilledBox(width-3, height-3, 1,1,1,1), new Vector2Int(2, 2));
 
         Utils.PutOnGrid(setup_setup_grid, strongWall, 2);
 
-        List<Vector2Int> border = Utils.PatternBox(15,13);
+        List<Vector2Int> border = Utils.PatternBox(width, height);
 
         Utils.PutOnGrid(setup_setup_grid, border, 2);
 
 
         //One of the 4 initial positions will be chosen for the Agent Player, the other ones will be filled by Non-Player Bomberman
-        List <Vector2Int> startPos = new List<Vector2Int> { new Vector2Int(1, 1), new Vector2Int(13, 1), new Vector2Int(13, 11), new Vector2Int(1, 11) };
+        List <Vector2Int> startPos = new List<Vector2Int> { new Vector2Int(1, 1), new Vector2Int(width-2, 1), new Vector2Int(width-2, height-2), new Vector2Int(1, height-2) };
         Utils.Shuffle<Vector2Int>(startPos, prng);
         for(int i =0; i < 4; i++)
         {
